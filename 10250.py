e56a18b380348f28859d60ee74a47d162c686c5a
DESKTOP-MI3N842\yx002 <yx00213@gmail.com>
2022-08-08 01:02:12 +0900

    코드 수정
    변경 사항
    1) list -> map(), split() 사용 : 코드 간결화
    2) 손님 순서가 층 수의 배수인 경우, roomNum 변화

--- 10250.py
+++ 10250.py
@@ -1,23 +1,11 @@
 ##10250-ACM호텔##
 T=int(input())
-arr1 = []
-arr2 = []
 roomNum=0
 
 for i in range(0,T):
-    for k in range(0,3):
-        value = int(input())
-        arr1.append(value)
-    arr2.append(arr1)
-    arr1=[]
-
-print("----결과----")      
-for i in range(0,T):
-    H=arr2[i][0]
-    W=arr2[i][1]
-    N=arr2[i][2]
-    if (N//H)+1 < 10:
-        roomNum = str(N%H)+str(0)+str((N//H)+1)
+    H, W, N =map(int,input().split())
+    if (N%H != 0):
+        roomNum = (N%H)*100+((N//H)+1)
     else:
-        roomNum = str(N%H)+str((N//H)+1)
+        roomNum = H*100+(N//H)
     print(roomNum)
